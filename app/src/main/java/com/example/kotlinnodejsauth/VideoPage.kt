c0d900efa3d54ba1e83ddf421ee9ce6788f55a38
leejusang <toy2437@naver.com>
2020-05-11 16:24:10 +0900

    영상 볼래?

--- app/src/main/java/com/example/kotlinnodejsauth/VideoPage.kt
+++ app/src/main/java/com/example/kotlinnodejsauth/VideoPage.kt
@@ -1,6 +1,5 @@
 package com.example.kotlinnodejsauth
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -8,109 +7,71 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlinnodejsauth.Common.Common
-import com.example.kotlinnodejsauth.Retrofit.ServiceApi
+import com.example.kotlinnodejsauth.Retrofit.VideoAPI
 import com.example.kotlinnodejsauth.Service.PicassoImageLoadingService
-//import com.example.kotlinnodejsauth.adapter.FeedAdapter
 import com.example.kotlinnodejsauth.adapter.MyVideoAdapter
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.fragment_feed.*
+import kotlinx.android.synthetic.main.fragment_video_page.*
 import ss.com.bannerslider.Slider
 import java.lang.StringBuilder
 
-/**
- * A simple [Fragment] subclass.
- */
-class FeedFragment : Fragment() {
+class VideoPage : Fragment() {
 
     var compositeDisposable = CompositeDisposable()
-    lateinit var myAPI: ServiceApi
+    lateinit var VideoAPI: VideoAPI
 
     override fun onStop() {
         super.onStop()
         compositeDisposable.clear()
     }
 
+    override fun onResume() {
+        super.onResume()
+        fetchVideo()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        //Init API
-        myAPI = Common.serviceapi
-
-
-
-
-
-        Slider.init(PicassoImageLoadingService(this))
-
-
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false)
+        return inflater.inflate(R.layout.fragment_video_page, container, false)
     }
 
-    override fun onResume() {
-        super.onResume()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-
-
-
-
-        swipe_refresh.setColorSchemeResources(
-            R.color.colorPrimary,
-            android.R.color.holo_orange_dark,
-            android.R.color.background_dark
-        )
-        swipe_refresh.setOnRefreshListener {
-            if (Common.isConnectedToInternet(activity)) {
-                fetchVideo()
-            } else {
-                Toast.makeText(context, "pleas check your connection", Toast.LENGTH_SHORT).show()
-            }
-
-        }
-
-
-        upload_page_button.setOnClickListener {
+        //영상볼래 게시물 등록하러가기 버튼
+        upload_Video.setOnClickListener {
             val Intent = Intent(context, UploadPage::class.java)
             startActivity(Intent)
         }
 
-        pic_gallery_btn.setOnClickListener {
-            val gallerypageintet = Intent(context, Gallerypage::class.java)
-            startActivity(gallerypageintet)
-        }
-
-
-
+        //Init API
+        VideoAPI = Common.api
+        Slider.init(PicassoImageLoadingService(this))
+        return
     }
-
 
     private fun fetchVideo() {
         compositeDisposable.add(
-            myAPI.videoList
+            VideoAPI.videoList
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({ videoList ->
                     Log.d("메인 비디오", videoList.toString())
-                    txt_video.text = StringBuilder("New Video(")
+                    txt_video2.text = StringBuilder("New Video(")
                         .append(videoList.size)
                         .append(")")
                     recycler_video.layoutManager = LinearLayoutManager(context)
-                    recycler_video.adapter = MyVideoAdapter(activity, videoList)
+                    recycler_video.adapter = MyVideoAdapter(context, videoList)
                 },
                     { thr ->
-                        Toast.makeText(activity, "" + thr.message, Toast.LENGTH_SHORT).show()
-
+//                        Toast.makeText(context, "" + thr.message, Toast.LENGTH_SHORT).show()
                     })
         )
-
     }
-
 }
-
