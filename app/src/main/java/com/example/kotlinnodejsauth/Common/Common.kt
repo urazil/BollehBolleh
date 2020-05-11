f47bed263c0c5f94bda9f9db93f66079f1850cba
leejusang <toy2437@naver.com>
2020-05-11 16:16:42 +0900

    Common

--- app/src/main/java/com/example/kotlinnodejsauth/Common/Common.kt
+++ app/src/main/java/com/example/kotlinnodejsauth/Common/Common.kt
@@ -1,15 +1,10 @@
 package com.example.kotlinnodejsauth.Common
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
-import androidx.fragment.app.FragmentActivity
 import com.example.kotlinnodejsauth.Retrofit.*
 import com.example.kotlinnodejsauth.data.CommentItem
+import com.example.kotlinnodejsauth.data.GalleryCommentItem
 import com.example.kotlinnodejsauth.data.Photo
 import com.example.kotlinnodejsauth.data.Video
-import retrofit2.create
 
 object Common {
 
@@ -17,34 +12,46 @@ object Common {
     var select_video: Video? = null
     var select_comment: CommentItem? = null
     var select_photo: Photo? = null
+    var select_gallery_comment : GalleryCommentItem? = null
     // SDK버젼에 따른 네트워크 연결상태 확인
-    fun isConnectedToInternet(context: FragmentActivity?): Boolean {
-        val cm = context!!.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (cm != null) {
-            if (Build.VERSION.SDK_INT < 23) {
-                val ni = cm.activeNetworkInfo
-                if (ni != null)
-                    return ni.isConnected && (ni.type == ConnectivityManager.TYPE_WIFI ||
-                            ni.type == ConnectivityManager.TYPE_MOBILE)
+//    fun isConnectedToInternet(context: FragmentActivity?): Boolean {
+//        val cm = context!!.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+//        if (cm != null) {
+//            if (Build.VERSION.SDK_INT < 23) {
+//                val ni = cm.activeNetworkInfo
+//                if (ni != null)
+//                    return ni.isConnected && (ni.type == ConnectivityManager.TYPE_WIFI ||
+//                            ni.type == ConnectivityManager.TYPE_MOBILE)
+//
+//            } else {
+//                val n = cm.activeNetwork
+//                if (n != null) {
+//                    val nc = cm.getNetworkCapabilities(n)
+//                    return nc.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+//                            nc.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+//                }
+//            }
+//        }
+//        return false
+//    }
 
-            } else {
-                val n = cm.activeNetwork
-                if (n != null) {
-                    val nc = cm.getNetworkCapabilities(n)
-                    return nc.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
-                            nc.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
-                }
-            }
-        }
-        return false
-    }
-
-
-    val serviceapi: ServiceApi
+    val api: VideoAPI
         get() {
             val retrofit = RetrofitClient.instance
-            return retrofit.create(ServiceApi::class.java)
+            return retrofit.create(VideoAPI::class.java)
         }
+
+    val apii: CommentAPI
+        get() {
+            val retrofit = RetrofitClient.instance
+            return retrofit.create(CommentAPI::class.java)
+        }
+
+    val photoapi: ServiceApi
+    get(){
+        val retrofit = RetrofitClient.instance
+        return retrofit.create(ServiceApi::class.java)
+    }
 
 
 
